25f3b69492972c24804c9942af0a800dd716b5fe
Valentyna Paniuk <Paniuk@rivne.itstep.org>
2020-01-13 14:00:07 +0200

    Classwork 20200113

--- Classwork20200113/Classwork20200113/Source20200113.cpp
+++ Classwork20200113/Classwork20200113/Source20200113.cpp
@@ -1,10 +1,103 @@
-#include <iostream>
-#include <string>
-
+#include<iostream>
+#include<string>
 using namespace std;
 
-int main()
+class Shooter
 {
+public:
+	string nameAmmunition;
+	int quantityAmmunition;
+
+	Shooter(string nameAmmunition = "weapon", int quantityAmmunition = 1)
+	{
+		this->nameAmmunition = nameAmmunition;
+		this->quantityAmmunition = quantityAmmunition;
+	}
+
+	void Fire() {
+		if (this->quantityAmmunition > 0) {
+			this->quantityAmmunition--;
+			cout << "fired by " << this->nameAmmunition << ", remains: " << this->quantityAmmunition << endl;
+		}
+		else cout << "Not enough ammunition" << endl;
+	}
+
+	void GiveAmmo(string nameAmmunition, int quantityAmmunition) {
+		this->nameAmmunition = nameAmmunition;
+		this->quantityAmmunition = quantityAmmunition;
+	}
+
+};
+
+class Archer : virtual public Shooter
+{
+public:
+	Archer(string nameAmmunition = "arrow", int quantityAmmunition = 1)
+	{
+		Archer::GiveAmmo(nameAmmunition, quantityAmmunition);
+	}
+
+	void Fire() {
+		cout << "Archer ";
+		Shooter::Fire();
+	}
+};
+
+class Ranger : virtual public Shooter
+{
+public:
+	Ranger(string nameAmmunition = "arrow", int quantityAmmunition = 1)
+	{
+		Ranger::GiveAmmo(nameAmmunition, quantityAmmunition);
+	}
+
+	void Fire() {
+		cout << "Ranger ";
+		Shooter::Fire();
+	}
+};
+
+class Catapult : public Shooter
+{
+public:
+	Catapult(string nameAmmunition = "stone", int quantityAmmunition = 1)
+	{
+		Catapult::GiveAmmo(nameAmmunition, quantityAmmunition);
+	}
+
+	void Fire() {
+		cout << "Catapult ";
+		Shooter::Fire();
+	}
+};
+
+class ArcherTower : public Archer, public Ranger
+{
+public:
+	ArcherTower(string nameAmmunition = "stone", int quantityAmmunition = 1) {}
+};
+
+class Fortress : public ArcherTower, public Catapult
+{
+public:
+	Fortress(int quantityArrow = 0, int quantityStone = 1) {
+		ArcherTower::quantityAmmunition = quantityArrow;
+		Catapult::quantityAmmunition = quantityStone;
+	}
+};
+
+int main() {
+
+	Fortress fortress(4, 4);
+
+	fortress.Archer::Fire();
+	fortress.Archer::Fire();
+	fortress.Archer::Fire();
+
+	fortress.Ranger::Fire();
+	fortress.Ranger::Fire();
+
+	fortress.Catapult::Fire();
 
 	system("pause");
 	return 0;
