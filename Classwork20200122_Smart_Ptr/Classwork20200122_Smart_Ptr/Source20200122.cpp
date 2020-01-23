5f7f3c0cdb086d5513551927728b817f907387a0
Valentyna Paniuk <Paniuk@rivne.itstep.org>
2020-01-23 12:17:45 +0200

    IP-address

--- Classwork20200122_Smart_Ptr/Classwork20200122_Smart_Ptr/Source20200122.cpp
+++ Classwork20200122_Smart_Ptr/Classwork20200122_Smart_Ptr/Source20200122.cpp
@@ -14,49 +14,41 @@ using namespace std;
 
 int main()
 {
+
+	char ip[16];
+	char temp[4];
+	int counter = 0;
 	bool exit = false;
-	char text[255];
-	int x = 0, y = 0;
-	cout << "************* EXERCISE 1 ********************" << endl;
+	cout << "****************** EXERCISE 1 ************************" << endl;
 	while (!exit)
 	{
-		cout << "Enter IP-address: ";
-		cin >> text;
-		for (int i = 0; i < strlen(text); i++)
-		{
-			
-			if (int(text[i]) != 46)
-			{
-				y += int(text[i]);
-				/*cout << "\nY = " << y << endl;*/
-				if (y >= 50 && y <= 156 || y >= 206 && y <= 312  ||  y >= 362  && y <= 468 || y >= 516)
-				{
-					cout << "\n" << text[i] << " ";
+		cout << "Enter IP: ";
+		cin >> ip;
+
+		int iterator = 0;
+		for (int i = 0; i < strlen(ip) + 1; i++) {
+			if (ip[i] == 46 || (i == strlen(ip) && counter == 3)) {
+				if (atoi(temp) <= 255) counter++;
+				for (int j = 0; j < 4; j++) {
+					temp[j] = '\0';
 					exit = true;
 				}
-				else
-				{
-					cout << "Not correact" << endl;
-				}
-
-				
-
+				iterator = 0;
 			}
+			else {
+				temp[iterator] = ip[i];
+				iterator++;
+			}
+		}
 
+		if (counter == 4) cout << "Correct" << endl;
+		else cout << "not correct" << endl;
 
-
-			/*else if (int(text[i]) == 46)
-			{
-				x++;
-				cout << "\nX (.) = " << x << " " << endl;
-
-			}*/
-
-			
+		for (int i = 0; i < strlen(ip); i++) {
+			cout << ip[i];
 		}
 		cout << endl;
 	}
-	//********************************************************************************
 
 	system("pause");
 	return 0;
